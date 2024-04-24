7796c8c804f18561d2596d9791302fb4e1d53318
PrinceSachan <sachanprince096@gmail.com>
2024-04-24 06:02:20 +0530

    added one more method

--- Cohort 2.0/class-2/FreqArr/prob1.js
+++ Cohort 2.0/class-2/FreqArr/prob1.js
@@ -1,16 +1,37 @@
-function freqnent(nums) {
-    let freq = nums.sort((a,b) => a - b);
-    let n = nums.length
-    let i = 0;
-    while(i < n){
-        let first_index = freq.indexOf(freq[i]);
-        let last_index = freq.lastIndexOf(freq[i]);
-        i = last_index
+// function freqnent(nums) {
+//     let freq = nums.sort((a,b) => a - b);
+//     let n = nums.length
+//     let i = 0;
+//     while(i < n){
+//         let first_index = freq.indexOf(freq[i]);
+//         let last_index = freq.lastIndexOf(freq[i]);
+//         i = last_index
 
-        // count the frequency
-        let eleFreq = last_index - first_index + 1;
-        console.log(freq[i], eleFreq);
-        i++ 
+//         // count the frequency
+//         let eleFreq = last_index - first_index + 1;
+//         console.log(freq[i], eleFreq);
+//         i++ 
+//     }
+// }
+// freqnent([1,2,1,1,3,3])
+
+function freqCount(arr) {
+    // find the max element in array
+    const max = Math.max(...arr)
+
+    // define frequency array
+    let freqArr = Array(max + 1).fill(0)
+
+    for(let i = 0; i < arr.length; i++){
+        freqArr[arr[i]] += 1
     }
+    console.log(freqArr)
+
+    // for (let j = 0; j< freqArr.length; j++) {
+    //     let value
+    //     freqArr[j] = value;
+    //     console.log(value)
+    // }
 }
-freqnent([1,2,1,1,3,3])
+
+freqCount([1,2,1,1,3,3])
